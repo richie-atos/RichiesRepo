d1101695547bfdb72e4c1c1ea92ef1d8f125d821
richie-atos <richard.harrison@atos.net>
2015-06-05 09:32:31 +0100

    Initial Test suites created.

--- TranscodingMarketplace/test/IntegrationTests/ApplicationSpec.scala
+++ TranscodingMarketplace/test/IntegrationTests/ApplicationSpec.scala
@@ -1,7 +1,8 @@
+package IntegrationTests
+
 import org.specs2.mutable._
 import org.specs2.runner._
 import org.junit.runner._
-
 import play.api.test._
 import play.api.test.Helpers._
 
---
+++ TranscodingMarketplace/test/allTests/AllTests.java
@@ -0,0 +1,15 @@
+package allTests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import controllers.ControllerTests;
+
+@RunWith(Suite.class)
+@SuiteClasses(
+{ControllerTests.class })
+public class AllTests
+{
+
+}
